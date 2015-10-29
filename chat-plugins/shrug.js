7066c103e8d361966e49f63c2613a19ad72608f4
megas4ever <yasharel17@yahoo.com>
2015-10-29 18:45:31 -0400

    Update shrug.js
    Add /shrughelp

--- chat-plugins/shrug.js
+++ chat-plugins/shrug.js
@@ -3,4 +3,7 @@ exports.commands = {
     if (!target) return '¯\\_(ツ)_/¯';
     if (target) return '¯\\_(ツ)_/¯ ' + target;
   },
+  shrughelp: function () {
+    return this.sendReply('/shrug - Makes you send '¯\_(ツ)_/¯' to the room.\n /shrug example sends '¯\_(ツ)_/¯ example'.
+  },
 };
