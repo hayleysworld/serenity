dc612114adc58df821ce33f31a3838f3b07642e8
megas4ever <yasharel17@yahoo.com>
2015-10-29 21:25:52 -0400

    Update shrug.js

--- chat-plugins/shrug.js
+++ chat-plugins/shrug.js
@@ -4,6 +4,6 @@ exports.commands = {
     if (target) return '¯\\_(ツ)_/¯ ' + target;
   },
   shrughelp: function () {
-    return this.sendReply('/shrug - Makes you send '¯\_(ツ)_/¯' to the room.\n /shrug example sends '¯\_(ツ)_/¯ example'.
+    return this.sendReply('/shrug - Makes you send \'¯\_(ツ)_/¯\' to the room.\n /shrug example sends \'¯\_(ツ)_/¯ example\'.');
   },
 };
